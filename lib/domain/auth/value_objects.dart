5843e275d6272ac9f676d649a09fef9610c793f0
Фаиз <galimow63@gmail.com>
2022-06-05 16:28:44 +0400

    ValueObjects (Email, Password) added

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+
+import '../core/failures.dart';
+import '../core/value_objects.dart';
+import '../core/value_validators.dart';
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(
+      value: validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._({required this.value});
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(
+      value: validatePassword(input),
+    );
+  }
+
+  const Password._({required this.value});
+}
