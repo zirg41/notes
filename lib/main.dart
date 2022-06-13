ad9e3c1a49df653699536e4f60a630fc9b45020b
Фаиз <galimow63@gmail.com>
2022-06-13 22:37:50 +0400

    using injection func + initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:notes/injection.dart';
+import 'package:notes/presentation/core/app_widget.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   configureInjection(Environment.prod);
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Empty Material App'),
-        ),
-        body: const Center(
-          child: Text('EmptyPage'),
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
